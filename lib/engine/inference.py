3c8d3d519cac87938e55156997791dd718f330c6
BrandonHan <brandon.hxiao@gmail.com>
2021-11-04 02:36:51 +0800

    Fix CUDA out of memory

--- lib/engine/inference.py
+++ lib/engine/inference.py
@@ -1,4 +1,5 @@
 import datetime
+import gc
 import logging
 import time
 
@@ -55,65 +56,81 @@ def compute_on_dataset_multiturn(model, data_loader, device):
     query_feats, query_ids, task_weights = [], [], []
     gallery_comp_feats, gallery_outfit_feats = [], []
 
-    for imgs in tqdm(
-        data_loader.dataset.get_all_imgs(data_loader.batch_sampler.batch_size)
-    ):
-        imgs = imgs.to(device)
-        with torch.no_grad():
+    with torch.no_grad():
+
+        for imgs in tqdm(
+            data_loader.dataset.get_all_imgs(data_loader.batch_sampler.batch_size)
+        ):
+            imgs = imgs.to(device)
             gallery_comp_feat, gallery_outfit_feat = model.extract_img_feature(
                 imgs, norm=False
             )
             gallery_comp_feats.append(gallery_comp_feat)
             gallery_outfit_feats.append(gallery_outfit_feat)
-    gallery_comp_feats = torch.cat(gallery_comp_feats, dim=0)
-    gallery_outfit_feats = torch.cat(gallery_outfit_feats, dim=0)
-    gallery_comp_feats_norm = model.norm_layer(gallery_comp_feats)
-    gallery_outfit_feats_norm = model.norm_layer(gallery_outfit_feats)
-    if len(gallery_outfit_feats_norm.shape) > 2:
-        gallery_comp_feats_norm = gallery_comp_feats_norm.mean((2, 3))
-        gallery_outfit_feats_norm = gallery_outfit_feats_norm.mean((2, 3))
+        gallery_comp_feats = torch.cat(gallery_comp_feats, dim=0)
+        gallery_outfit_feats = torch.cat(gallery_outfit_feats, dim=0)
+        gallery_comp_feats_norm = model.norm_layer(gallery_comp_feats)
+        gallery_outfit_feats_norm = model.norm_layer(gallery_outfit_feats)
+        if len(gallery_outfit_feats_norm.shape) > 2:
+            gallery_comp_feats_norm = gallery_comp_feats_norm.mean((2, 3))
+            gallery_outfit_feats_norm = gallery_outfit_feats_norm.mean((2, 3))
 
-    for batch_data in tqdm(data_loader):
-        imgs = batch_data["source_images"].to(device)
-        query_ids.append(batch_data["meta_info"]["target_image_ids"])
+        for batch_data in tqdm(data_loader):
+            imgs = batch_data["source_images"].to(device)
+            query_ids.append(batch_data["meta_info"]["target_image_ids"])
 
-        # For each turn
-        for text, text_length in zip(batch_data["text"], batch_data["text_lengths"]):
-            text = text.to(device)
-            text_length = text_length.to(device)
-            with torch.no_grad():
-                query_feat, weights = model.compose_img_text(
-                    imgs, text, text_length, return_weights=True
-                )
-                # Greedy search
-                max_comp_idx = torch.argmax(
-                    query_feat @ gallery_comp_feats_norm.t(), dim=1
-                )
-                max_outfit_idx = torch.argmax(
-                    query_feat @ gallery_outfit_feats_norm.t(), dim=1
-                )
-                max_idx = weights[:, 0] * max_comp_idx + weights[:, 1] * max_outfit_idx
-                max_idx = max_idx.long()
-                imgs = (gallery_comp_feats[max_idx], gallery_outfit_feats[max_idx])
+            # For each turn
+            for text, text_length in zip(
+                batch_data["text"], batch_data["text_lengths"]
+            ):
+                text = text.to(device)
+                text_length = text_length.to(device)
+                with torch.no_grad():
+                    query_feat, weights = model.compose_img_text(
+                        imgs, text, text_length, return_weights=True
+                    )
+                    # Greedy search
+                    max_comp_idx = torch.argmax(
+                        query_feat @ gallery_comp_feats_norm.t(), dim=1
+                    )
+                    max_outfit_idx = torch.argmax(
+                        query_feat @ gallery_outfit_feats_norm.t(), dim=1
+                    )
+                    max_idx = (
+                        weights[:, 0] * max_comp_idx + weights[:, 1] * max_outfit_idx
+                    )
+                    max_idx = max_idx.long()
+                    imgs = (gallery_comp_feats[max_idx], gallery_outfit_feats[max_idx])
 
-        query_feats.append(query_feat)
-        task_weights.append(weights)
+            query_feats.append(query_feat)
+            task_weights.append(weights)
 
-    query_feats = torch.cat(query_feats, dim=0)
-    task_weights = torch.cat(task_weights, dim=0)
-    comp_idx = task_weights[:, 0] == 1
-    outfit_idx = task_weights[:, 1] == 1
-    comp_similarity = query_feats[comp_idx] @ gallery_comp_feats_norm.t()
-    outfit_similarity = query_feats[outfit_idx] @ gallery_outfit_feats_norm.t()
-    similarity = torch.cat((comp_similarity, outfit_similarity), dim=0)
-    query_ids = torch.cat(query_ids, dim=0)
-    query_ids = torch.cat((query_ids[comp_idx], query_ids[outfit_idx]), dim=0)
+        query_feats = torch.cat(query_feats, dim=0)
+        task_weights = torch.cat(task_weights, dim=0)
+        comp_idx = task_weights[:, 0] == 1
+        outfit_idx = task_weights[:, 1] == 1
+        comp_similarity = query_feats[comp_idx] @ gallery_comp_feats_norm.t()
+        outfit_similarity = query_feats[outfit_idx] @ gallery_outfit_feats_norm.t()
 
-    results_dict = dict(
-        query_ids=query_ids,
-        gallery_ids=torch.tensor(list(data_loader.dataset.all_img_ids.values())),
-        similarity=similarity,
-    )
+        del gallery_comp_feats_norm
+        del gallery_outfit_feats_norm
+        del gallery_comp_feats
+        del gallery_outfit_feats
+
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
+
+        similarity = torch.cat((comp_similarity, outfit_similarity), dim=0)
+
+        query_ids = torch.cat(query_ids, dim=0)
+        query_ids = torch.cat((query_ids[comp_idx], query_ids[outfit_idx]), dim=0)
+
+        results_dict = dict(
+            query_ids=query_ids,
+            gallery_ids=torch.tensor(list(data_loader.dataset.all_img_ids.values())),
+            similarity=similarity,
+        )
 
     return results_dict
 
